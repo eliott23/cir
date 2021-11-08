479d5701da5dda76e4a27a01bb0613c6936cc91d
Abderrahmane Ababachir <aababach@e2r12p2.1337.ma>
2021-11-08 17:13:51 +0100

    fv

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 17:11:55 by aababach          #+#    #+#             */
+/*   Updated: 2021/11/08 17:12:52 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	unsigned char *d;
-	unsigned char *s;
+	unsigned char	*d;
+	unsigned char	*s;
 
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
@@ -15,24 +27,4 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 		n--;
 	}
 	return (dst);
-}
-
-int	main()
-{
-/*	unsigned char dst1[] = "Nko";
-	unsigned char dst2[] = "Nko";
-	unsigned char src1[] = "skdfg";
-	unsigned char src2[] = "skdfg";
-*/
-	int	dst1[] = {1, 2 , 3};
-	int	dst2[] = {1, 2 , 3};
-	int	src1[] = {6, 5 , 4};
-	int	src2[] = {6, 5 , 4};
-	int	i = 3;
-	while (i)
-	{
-		pi(memcpy(dst1,src1,4));
-		i--;
-	}
-	pi(ft_memcpy(dst2,src2,4));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 18:15:52 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/08 16:00:07 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/08 17:13:43 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ int	ft_isprint(int c); //check
 size_t	ft_strlen(const char *s); // check
 void	*ft_memset(void *b, int c, size_t len); //check
 void	ft_bzero(void *s, size_t n); // check
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n); //check
+
 #endif
