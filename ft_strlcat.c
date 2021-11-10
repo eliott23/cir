5c4066c518c389407e42fc9060c4e50dab98e631
Abderrahmane Ababachir <aababach@e1r9p3.1337.ma>
2021-11-10 12:57:20 +0100

    work

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,7 +3,6 @@
 size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
 {
 	size_t	return_value;
-	size_t	d_i;
 	size_t	s_i;
 	size_t	s_l;
 	size_t	d_l;
@@ -16,19 +15,28 @@ size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize
 	while (src[s_l])
 		s_l++;
 	return_value = s_l + d_l;
-	d_i = d_l;
-	ps("this is the very last index and also the length of dest ");
-	printf("%zu\n",d_i);
 	if (dstsize)
 	{
-		while (d_i < (dstsize - 1) && src[s_i])
+		while (d_l < (dstsize - 1) && src[s_i])
 		{
-			ps("it went here ");
-			dst[d_i] = src[s_i];
+			dst[d_l] = src[s_i];
 			s_i++;
-			d_i++;
+			d_l++;
 		}
-		dst[d_i] = '\0';
+		dst[d_l] = '\0';
 	}
 	return (return_value);
+}
+
+int	main()
+{
+	char dst[5] = "Hello";
+	char dst1[5] = "Hello";
+	char src[] = "zeb";
+	char src1[] = "zeb";
+
+	ft_strlcat(dst,src,5);
+	strlcat(dst1,src1,5);
+	ps(dst);
+	ps(dst1);
 }
