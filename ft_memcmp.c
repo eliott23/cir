81016133d8369f3792025fecc7c575a61c7b7f19
Abderrahmane Ababachir <aababach@e2r6p12.1337.ma>
2021-11-12 19:00:15 +0100

    work

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 15:35:50 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/12 18:47:52 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/12 18:59:15 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		ps("this is i ");
-		pi(i);
 		if (p1[i] != p2[i])
-		{
-			ps("this is p1[i] ");
-			pi(p1[i]);
-			ps("this is p2[i] ");
-			pi(p2[i]);
 			return (p1[i] - p2[i]);
-		}
 		i++;
 	}
 	return (0);
@@ -40,18 +32,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 int	main()
 {
-	char s1[4] = "ZEBI";
-	char *p1 = s1 + 4;
-	char s2[4] = "ZEBI";
-	char s3[4] = "ZEBI";
-	char *p3 = s3 + 4;
-	char *p2 = s2 + 4;
-	pi(*p1);
-	pi(*p2);
-	pi(*p3);
+	char s1[] = "ZEBI";
+	char *p1 = s1 + 5;
+	char s2[] = "ZEBI";
+	char *p2 = s2 + 5;
 	memset(p1,65,3);
-	pi(*p1);
-	pi(ft_memcmp(s1,s2,5));
-	ps("\n");
-	pi(memcmp(s1,s2,5));
 }
---
+++ testprograms/test_memcmp.c
@@ -0,0 +1,8 @@
+int	main()
+{
+	char s1[] = "ZEBI";
+	char *p1 = s1 + 5;
+	char s2[] = "ZEBI";
+	char *p2 = s2 + 5;
+	memset(p1,65,3);
+}
