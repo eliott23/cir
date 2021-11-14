ae2b972f88b173c7458ed7ab231c16de1b972c4e
Abderrahmane Ababachir <aababach@e1r10p11.1337.ma>
2021-11-14 14:41:08 +0100

    work

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 17:11:55 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/08 17:12:52 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/14 14:34:02 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
+	if (dst == NULL && src == NULL)
+		return (0);
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
 	while (n)
