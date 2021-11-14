d01bc5bf4b46fdac21bceab83a13b070717fa475
Abderrahmane Ababachir <aababach@e1r10p11.1337.ma>
2021-11-14 19:37:21 +0100

    work

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:58:10 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/14 19:09:46 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/14 19:32:32 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	size_t	d_l;
 
 	s_i = 0;
-	s_l = 0;
-	d_l = 0;
-	while (dst[d_l])
-		d_l++;
-	while (src[s_l])
-		s_l++;
+	s_l = ft_strlen(src);
+	d_l = ft_strlen(dst);
+	if (dstsize <= d_l)
+		return (s_l + dstsize);
 	return_value = s_l + d_l;
 	if (dstsize)
 	{
@@ -39,12 +37,4 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 		dst[d_l] = '\0';
 	}
 	return (return_value);
-}
-int	main()
-{
-	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
-	char buff1[0xF00] = "there is no stars in the sky";
-	char buff2[0xF00] = "there is no stars in the sky";
-
-	pi(strlen(str));
 }
