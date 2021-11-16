430577ea17c4789bcb3e0f911ba46a87bd2a8724
Abderrahmane Ababachir <aababach@e1r12p12.1337.ma>
2021-11-16 16:08:58 +0100

    work

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 14:10:32 by aababach          #+#    #+#             */
+/*   Updated: 2021/11/16 15:25:47 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 static int	ft_is_in(char c, char *s)
@@ -14,38 +26,32 @@ static int	ft_is_in(char c, char *s)
 	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	len;
 	char	*str;
 	size_t	min_i;
 	size_t	max_i;
 
+	if (!s1)
+		return (NULL);
+	if (ft_strlen(s1) == 0)
+		return (ft_strdup(""));
 	len = ft_strlen(s1);
 	max_i = len - 1;
 	min_i = 0;
-	str = malloc(sizeof(char) * len + 1);
+	while (ft_is_in(s1[min_i], (char *)set))
+		min_i++;
+	while (ft_is_in(s1[max_i], (char *)set))
+		max_i--;
+	if (min_i == ft_strlen(s1))
+		return (ft_strdup(""));
+	str = malloc(sizeof(char) * (max_i - min_i + 2));
 	if (!str)
 		return (0);
-	while (ft_is_in(s1[min_i],(char *)set))
-		min_i++;
-	while (ft_is_in(s1[max_i],(char *)set))
-		max_i--;
+	len = 0;
 	while (min_i <= max_i)
-	{
-		ps("it went here ");
-		str[min_i] = s1[min_i];
-		pc(str[min_i],0);
-		min_i++;
-	}
-	str[min_i] = 0;
+		str[len++] = s1[min_i++];
+	str[len] = 0;
 	return (str);
-}
-
-int	main()
-{
-	char x[] = "123";
-	char s[] = "312JIJIIII4231";
-
-	ps(ft_strtrim(s,x));
 }
