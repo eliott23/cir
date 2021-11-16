04015694fa7b1e351aabc5d25a0fd44b9277ed59
Abderrahmane Ababachir <aababach@e2r10p5.1337.ma>
2021-11-17 00:56:46 +0100

    work

---
+++ ft_split.c
@@ -0,0 +1,62 @@
+#include "libft.h"
+
+static int	ft_count_sep(char const *s, char c)
+{
+	int	i;
+	int	sc;
+
+	sc = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1])
+			sc++;
+		i++;
+	}
+	return (sc);
+}
+
+static int	ft_count_length(char const *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+char **ft_split(char const *s, char c);
+{
+	char **str;
+	int	i;
+	int	a;
+	int	b;
+
+	i = 0;
+	str = malloc (sizeof(char) * ft_count_sep(s,c) - 1);
+	if (!str == NULL)
+		return (0)
+	while (i < ft_count_sep(s,c) - 1)
+	{
+		str[i] = malloc (sizeof(char) * ft_count_length(&s[a],c) + 1);
+		if (!str[i])
+			return (0);
+		i++;
+		a += ft_count_length(&s[a],c) - a + 1;
+	}
+	a = 0;
+	i = 0;
+	b = 0;
+	while (i < ft_count_length(s,c) - 1)
+	{
+		while (b < ft_count_length(s[a],c))
+		{
+			str[i][b] = s[b];
+			b++;
+		}
+		i++;
+		a+= ft_count_length(&s[a],c) - a + 1;
+	}
+
+}
