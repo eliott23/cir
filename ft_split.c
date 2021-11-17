43c73960dc3b1ea9dbe56a68b2c41a77b05f0058
Abderrahmane Ababachir <aababach@e1r2p10.1337.ma>
2021-11-17 20:05:48 +0100

    work

--- ft_split.c
+++ ft_split.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 18:56:49 by aababach          #+#    #+#             */
+/*   Updated: 2021/11/17 20:05:43 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 static int	ft_count_sep(char const *s, char c)
@@ -10,14 +22,8 @@ static int	ft_count_sep(char const *s, char c)
 	while (s[i])
 	{
 		if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
-		{
 			sc++;
-			i++;
-			while (s[i] == c)
-				i++;
-		}
-		else
-			i++;
+		i++;
 	}
 	return (sc);
 }
@@ -52,6 +58,16 @@ static int	ft_next_substr(char const *s, char c)
 	}
 	return (0);
 }
+static void	ft_fuck_norms(int *a, char **str, char *s, char c)
+{
+	char	**str;
+
+	*a
+	if (!s)
+		return (0);
+	while (s[*a] == c)
+		(*a)++;
+}
 
 char **ft_split(char const *s, char c)
 {
@@ -59,49 +75,31 @@ char **ft_split(char const *s, char c)
 	int	i;
 	int	a;
 	int	b;
+	int	len;
 
 	i = 0;
+	b = 0;
 	a = 0;
+	if (!s)
+		return (0);
 	str = malloc (sizeof(char *) * (ft_count_sep(s,c) + 1));
 	if (!str)
 		return (0);
+	while (s[a] == c)
+		a++;
 	while (i < ft_count_sep(s,c))
 	{
-		str[i] = malloc(sizeof(char) * ft_count_length(&s[a],c) + 1);
+		str[i] = malloc (sizeof(char) * ft_count_length(&s[a],c) + 1);
 		if (!str[i])
 			return (0);
-		i++;
-		a = ft_next_substr(&s[a],c);
-	}
-	str[i] = 0;
-	a = 0;
-	i = 0;
-	while (i < ft_count_sep(s,c))
-	{
 		b = 0;
-		while (b < ft_count_length(&s[a],c))
-		{
-			str[i][b] = s[b];
-			b++;
-		}
+		len = ft_count_length(&s[a],c);
+		while (b < len)
+			str[i][b++] = s[a++];
 		str[i][b] = 0;
 		i++;
-		a = ft_next_substr(&s[a],c);
+		a = ft_next_substr(&s[a],c) + a;
 	}
+	str[i] = 0;
 	return (str);
-}
-
-int	main()
-{
-	char **str;
-	int	i = 0;
-
-//	printf("%d", ft_count_length("dfg     sdg  dfg  dg", ' '));
-	str = ft_split("   zebi zebi zebi",' ');
-	while (str[i])
-	{
-		printf("%s\n", str[i]);
-		i++;
-	}
-	free(str);
 }
