274d169eabfe16618146ae3956e26247221a7f66
Abderrahmane Ababachir <aababach@e2r7p3.1337.ma>
2021-11-19 10:14:20 +0100

    work

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 18:15:52 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/19 09:50:13 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/19 10:08:56 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,21 +30,22 @@ void	*ft_memset(void *b, int c, size_t len); //check
 void	ft_bzero(void *s, size_t n); // check
 void	*ft_memcpy(void *dst, const void *src, size_t n); //check
 void	*ft_memmove(void *dst, const void *src, size_t len); //check
-size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize); //check
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize); //check
-int ft_toupper(int c); //check
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); //check
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize); //check
+int		ft_toupper(int c); //check
 char	*ft_strchr(const char *s, int c); //check
 char	*ft_strrchr(const char *s, int c); //check
-int	ft_strncmp(const char *s1, const char *s2, size_t n); //check
+int		ft_strncmp(const char *s1, const char *s2, size_t n); //check
 void	*ft_memchr(const void *s, int c, size_t n); //check
-int	ft_memcmp(const void *s1, const void *s2, size_t n); //check
+int		ft_memcmp(const void *s1, const void *s2, size_t n); //check
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len); //check
-int ft_atoi(const char *str); //check
+int		ft_atoi(const char *str); //check
 char	*ft_strdup(const char *s1); //check
 char	*ft_substr(char const *s, unsigned int start, size_t len); //check
-char *ft_strjoin(char const *s1, char const *s2); //check
-char *ft_strtrim(char const *s1, char const *set); //check
-char    **ft_split(char const *s, char c); //check
-char    *ft_itoa(int n); //check
+char	*ft_strjoin(char const *s1, char const *s2); //check
+char	*ft_strtrim(char const *s1, char const *set); //check
+char	**ft_split(char const *s, char c); //check
+char	*ft_itoa(int n); //check
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char)); //check
+void	ft_striteri(char *s, void (*f)(unsigned int, char*)); //check
 #endif
