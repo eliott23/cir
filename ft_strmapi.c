16d1511cc82d190f8c56184188686e7ba0fa6249
Abderrahmane Ababachir <aababach@e2r7p3.1337.ma>
2021-11-19 10:20:28 +0100

    work

---
+++ ft_strmapi.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = ft_strdup(s);
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		str[i] = (*f)(i
+		i++;
+	}
+}
