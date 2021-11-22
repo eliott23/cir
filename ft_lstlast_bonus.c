084b05285c5c686e1111b89ebf145c77c1947519
Abderrahmane Ababachir <aababach@e1r11p12.1337.ma>
2021-11-22 19:47:38 +0100

    work

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 19:05:47 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/22 19:06:03 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/22 19:46:34 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	while (lst)
 		lst = lst->next;
-	return (t_list);
+	return (lst);
 }
