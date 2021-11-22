d26b894e8dcecaa1a3b72fd17d66d474d1ec4489
Abderrahmane Ababachir <aababach@e1r11p12.1337.ma>
2021-11-22 16:54:42 +0100

    work

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:48:17 by aababach          #+#    #+#             */
-/*   Updated: 2021/11/22 15:55:38 by aababach         ###   ########.fr       */
+/*   Updated: 2021/11/22 16:54:17 by aababach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,10 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	first;
-	int	i = 10;
-	first.content = &i;
-	first.next = NULL;
-	t_list	*head = &first;
+	t_list	*head;
 
-//	head = malloc(sizeof(t_list));
+	head = malloc(sizeof(t_list));
 	head->content = content;
 	head->next = NULL;
-//	printf("%d",*(int *)content);
 	return (head);
-}
-
-int	main()
-{
-	int	a = 9;
-	ft_lstnew(&a);
 }
---
+++ test/test.c
@@ -0,0 +1,10 @@
+int	ft_test()
+{
+	int	a;
+	a = 10;
+	return (a);
+}
+int	main()
+{
+	ft_test();
+}
